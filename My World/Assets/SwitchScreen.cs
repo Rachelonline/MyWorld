ce013c4cb6362fab353e54d532fc9513cf13944b
Rachel Hu <rachelhu09@gmail.com>
2020-06-29 18:05:50 -0700

    Added the basic screens and stuff

---
+++ My World/Assets/SwitchScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScreen : MonoBehaviour
+{
+    public void ToTaskScreen()
+    {
+        SceneManager.LoadScene("TaskScreen");
+    }
+
+    public void ToWorldScreen()
+    {
+        SceneManager.LoadScene("WorldScreen");
+    }
+
+    public void ToHomeScreen()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+}
